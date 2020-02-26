6d0583b623172253e9c0e781981900b820c59bf2
Vepnar <agdehaan@gmail.com>
2020-02-26 23:13:06 +0100

    Next beta

--- login/users.py
+++ login/users.py
@@ -1,25 +1,58 @@
 import json
 import session
+
 from mongoengine import *
 from sanic import response
 from sanic import Blueprint
+from util import *
 
-from util import format_html, hash_password
+from datetime import datetime
+
 
 BLUEPRINT = Blueprint('users', url_prefix='/users')
 
 
-class SessionModel(Document):
+class UserModel(Document):
     """Store session information."""
     email = StringField(required=True, unique=True, max_length=32)
-    passsword = BinaryField(required=True, max_length=60)
+    password = BinaryField(required=True, max_length=60)
     created = DateTimeField(default=datetime.utcnow)
     last_login = DateTimeField(default=datetime.utcnow)
-    role = StringField(required=True, default='member')    
+    role = StringField(required=True, default='member')   
 
 @BLUEPRINT.get('/register')
 async def register(request):
     return await format_html('register.html')
+
+@BLUEPRINT.post('/register')
+async def register_post(request):
+    required = ['email', 'pwd-1', 'pwd-2', 'accept']
+    if not all(item in request.form for item in required):
+        return await format_html('register.html', error_message='Please accept our TOS')
+
+    email = request.form.get('email', '').lower()
+    password = request.form.get('pwd-1')
+    password2 = request.form.get('pwd-2')
+    accept = request.form.get('accept')
+
+    
+    message = can_register_user(email, password, password2, accept)
+    if message is not None:
+        return await format_html(
+            'register.html', error_message=message, email=email
+            )
+
+    if UserModel.objects(email=email):
+        return await format_html(
+            'register.html', error_message='This email is already registed', email=email
+            )
+
+    hashed_password = hash_password(password)
+    new_user = UserModel(email=email, password=hashed_password)
+    new_user.save()
+
+    request['session']['user_id'] = new_user.id
+    return await format_html('register.html',error_message=new_user.id)
 
 @BLUEPRINT.get('/')
 async def index(request):
@@ -28,9 +61,15 @@ async def index(request):
 @BLUEPRINT.get('/test')
 async def test(request):
     sessions = session.SessionModel.objects.count()
+    users = UserModel.objects.count()
     does_have_session = request.get('session')
-    hashed = hash_password(b'Hallo')
-    error = f'Amount of TRASH: {sessions}<br>session: {does_have_session}<br>hash: {hashed} {len(hashed)}'
+    user_id = request['session'].get('user_id')
+    error = f'''
+    Amount of session: {sessions}<br>
+    Amount of users: {users}<br>
+    session: {does_have_session}<br>
+    user_id: {user_id}'''
+
     return await format_html('error.html', error_code=200, error_message=error)
 
 @BLUEPRINT.get('/login')
--- login/util.py
+++ login/util.py
@@ -1,3 +1,4 @@
+import re
 import jinja2
 import bcrypt
 from sanic import response
@@ -16,6 +17,15 @@ async def format_html(html_file: str, **kwargs):
     return response.html(formatted_template)
 
 def is_logged_in(request):
+    """Checks if the user is logged in
+    
+    Args:
+        Request
+
+    Returns:
+        False when the user is not logged in
+        User_ID when the user is logged in
+    """
     if not request.get('session'):
         return False
     
@@ -24,7 +34,8 @@ def is_logged_in(request):
         return False
 
 def hash_password(password):
-    return bcrypt.hashpw(password, bcrypt.gensalt(10))
+    password = password.encode('utf-8')
+    return bcrypt.hashpw(password, bcrypt.gensalt(12))
 
 def check_password(password, hash):
     if bcrypt.checkpw(password, hashed):
@@ -33,3 +44,42 @@ def check_password(password, hash):
 
 def sanatize_html(string):
     return SANITIZER.sanitize(string)
+
+def is_valid_email(string):
+    if re.match(r'[^@]+@[^@]+\.[^@]+', string):
+        return True
+    return False
+
+def is_valid_password(password):
+    # Check length
+    if 5 > len(password): 
+        return False
+
+    # Check if there is a digit
+    if not any(char.isdigit() for char in password):
+        return False
+    
+    # Check if there is a non digit or alpha (disabled)
+    #if not any(char.isalnum() for char in password):
+    #    return False
+
+    # Check if there is an uppercase character
+    if not any(char.isupper for char in password):
+        return False
+
+    # Check if there is a space
+    if any(char.isspace() for char in password):
+        return False
+    return True
+
+def can_register_user(email, password, password2, accept):
+  if password != password2:
+    return 'passwords do not match!'
+
+    if accept != 'on':
+        return 'Please accept our TOS'
+    if not is_valid_email(email):
+        return 'Please enter a valid Email address'
+
+    if not is_valid_password(password):
+        return 'Your passwords needs to be longer than 6 characters and need atleast one number and capital letter'
